0b1fa2eac7a6d7e457c0cde494be302c2c178bbe
Sandra Kiefer <sandra.kiefer@student.hs-rm.de>
2020-05-05 10:05:00 +0200

    aufgabe02

--- src/main/java/de/hsrm/mi/web/skief001/login/LoginController.java
+++ src/main/java/de/hsrm/mi/web/skief001/login/LoginController.java
@@ -1,13 +1,29 @@
 package de.hsrm.mi.web.skief001.login;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
     
+    Logger logger = LoggerFactory.getLogger(LoginController.class);
+
     @GetMapping("/login")
     public String login_get() {
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String login_post(Model m, @RequestParam String username, @RequestParam String password) {
+        String correct = username + username.length();
+        if (!password.equals(correct)) {
+            m.addAttribute("hinweis", String.format("Hinweis: Das korrekte Passwort für %s ist %s, nicht %s!", username, correct, password));
+        }
         return "login";
     }
 
