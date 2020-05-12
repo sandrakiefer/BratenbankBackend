cf7e954c1891bab998313829218111e4ae38de61
Sandra Kiefer <sandra.kiefer@student.hs-rm.de>
2020-05-12 23:29:27 +0200

    problem

--- src/main/java/de/hsrm/mi/web/skief001/bratboerse/BratenAngebotController.java
+++ src/main/java/de/hsrm/mi/web/skief001/bratboerse/BratenAngebotController.java
@@ -2,11 +2,12 @@ package de.hsrm.mi.web.skief001.bratboerse;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
-
+import javax.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,14 +19,14 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 public class BratenAngebotController {
 
     Logger logger = LoggerFactory.getLogger(BratenAngebotController.class);
+    ArrayList<BratenDaten> angebote = new ArrayList<BratenDaten>();
     
     @ModelAttribute("angebote")
     public void initListe(Model m) {
-        ArrayList<BratenDaten> lst = new ArrayList<BratenDaten>();
-        lst.add(new BratenDaten("Jöndhard", "Vollradisroda", LocalDate.of(2020, 5, 6), "leckerer Grünkohlbraten"));
-        lst.add(new BratenDaten("Friedfert", "Arensch", LocalDate.of(2020, 5, 31), "Palatschinken aus Öl"));
-        lst.add(new BratenDaten("Joghurta", "Diedelingen", LocalDate.of(2020, 5, 7), "frischer Gummibärbraten"));
-        m.addAttribute("angebote", lst);
+        angebote.add(new BratenDaten("Jöndhard", "Vollradisroda", LocalDate.of(2020, 5, 6), "leckerer Grünkohlbraten"));
+        angebote.add(new BratenDaten("Friedfert", "Arensch", LocalDate.of(2020, 5, 31), "Palatschinken aus Öl"));
+        angebote.add(new BratenDaten("Joghurta", "Diedelingen", LocalDate.of(2020, 5, 7), "frischer Gummibärbraten"));
+        m.addAttribute("angebote", angebote);
     }
 
     @GetMapping("/angebot")
@@ -40,19 +41,24 @@ public class BratenAngebotController {
     }
 
     @PostMapping("/angebot/neu")
-    public String postFrom(@ModelAttribute("angebotfrom") BratenDaten angebotform, @ModelAttribute("angebote") ArrayList<BratenDaten> angebote) {
+    public String postFrom(Model m, @Valid @ModelAttribute("angebotfrom") BratenDaten angebotform, BindingResult bratendatenError) {
+        if (bratendatenError.hasErrors()) {
+            m.addAttribute("angebotform", angebotform);
+            logger.info(bratendatenError.getFieldError("name").getDefaultMessage());
+            return "angebote/bearbeiten";
+        }
         angebote.add(angebotform);
         return "redirect:/angebot";
     }
 
     @GetMapping("/angebot/{id}/del")
-    public String delete(@PathVariable int id, @ModelAttribute("angebote") ArrayList<BratenDaten> angebote) {
+    public String delete(@PathVariable int id) {
         angebote.remove(id-1);
         return "redirect:/angebot";
     }
 
     @GetMapping("/angebot/{id}")
-    public String edit(Model m, @PathVariable int id, @ModelAttribute("angebote") ArrayList<BratenDaten> angebote) {
+    public String edit(Model m, @PathVariable int id) {
         m.addAttribute("angebotform", angebote.get(id-1));
         angebote.remove(id-1);
         return "angebote/bearbeiten";
--- src/main/java/de/hsrm/mi/web/skief001/bratboerse/BratenDaten.java
+++ src/main/java/de/hsrm/mi/web/skief001/bratboerse/BratenDaten.java
@@ -1,20 +1,33 @@
 package de.hsrm.mi.web.skief001.bratboerse;
 
 import java.time.LocalDate;
+import javax.validation.constraints.Future;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.DateTimeFormat.ISO;
 
 public class BratenDaten {
+    
+    @Size(min=3, max=80, message="Name muss mindestens {min} Zeichen und maximal {max} Zeichen haben")
+    @NotNull(message="Name muss angegeben werden")
+    private String name;
+
+    @NotNull(message="Abholort muss angegeben werden")
+    private String abholort;
+
+    @DateTimeFormat(iso=ISO.TIME)
+    @Future
+    @NotNull(message="Datum muss angegeben werden")
+    private LocalDate haltbarbis;
+
+    @Size(max=80, message="Beschreibung darf nicht länger als {max} Zeichen haben")
+    @NotNull(message="Beschreibung muss angegeben werden")
+    private String beschreibung;
 
     Logger logger = LoggerFactory.getLogger(BratenDaten.class);
-    
-    private String name;
-    private String abholort;
-    @DateTimeFormat(iso=ISO.TIME)
-    private LocalDate haltbarbis;
-    private String beschreibung;
 
     public BratenDaten(String name, String abholort, LocalDate haltbarbis, String beschreibung) {
         this.name = name;
