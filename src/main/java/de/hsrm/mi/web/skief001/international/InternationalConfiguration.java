141fef30ca792fc32d7d6a5f0006462064d5c6d6
Sandra Kiefer <sandra.kiefer@student.hs-rm.de>
2020-05-14 23:02:22 +0200

    aufgabe05 (funktioniert leider nicht)

---
+++ src/main/java/de/hsrm/mi/web/skief001/international/InternationalConfiguration.java
@@ -0,0 +1,33 @@
+package de.hsrm.mi.web.skief001.international;
+
+import java.util.Locale;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+@Configuration
+public class InternationalConfiguration implements WebMvcConfigurer {
+    @Bean
+    public LocaleResolver localResolver() {
+        SessionLocaleResolver resolver = new SessionLocaleResolver();
+        resolver.setDefaultLocale(Locale.GERMANY);
+        return resolver;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
+        interceptor.setParamName("sprache");
+        return interceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
+}
