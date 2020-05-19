39539b3dd9c7a45406c72cd99f2351de2bdbcf73
supermoewe <sandra.kiefer@student.hs-rm.de>
2020-05-19 23:38:25 +0200

    aufgabe03

---
+++ src/main/java/de/hsrm/mi/web/bratenbank/benutzer/Benutzer.java
@@ -0,0 +1,95 @@
+package de.hsrm.mi.web.bratenbank.benutzer;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Version;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Benutzer {
+    
+    @NotNull
+    private String loginname;
+
+    @NotNull
+    @Size(min=3)
+    private String passwort;
+
+    @NotNull
+    private String vollname;
+
+    private boolean nutzungsbedingungenok;
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @Version
+    private long version;
+
+    public String getLoginname() {
+        return loginname;
+    }
+
+    public void setLoginname(String loginname) {
+        this.loginname = loginname;
+    }
+
+    public String getPasswort() {
+        return passwort;
+    }
+
+    public void setPasswort(String passwort) {
+        this.passwort = passwort;
+    }
+
+    public String getVollname() {
+        return vollname;
+    }
+
+    public void setVollname(String vollname) {
+        this.vollname = vollname;
+    }
+
+    public boolean isNutzungsbedingungenok() {
+        return nutzungsbedingungenok;
+    }
+
+    public void setNutzungsbedingungenok(boolean nutzungsbedingungenok) {
+        this.nutzungsbedingungenok = nutzungsbedingungenok;
+    }
+
+    @Override
+    public String toString() {
+        return "Benutzer [loginname=" + loginname + ", nutzungsbedingungenok=" + nutzungsbedingungenok + ", passwort="
+                + passwort + ", vollname=" + vollname + "]";
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((loginname == null) ? 0 : loginname.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Benutzer other = (Benutzer) obj;
+        if (loginname == null) {
+            if (other.loginname != null)
+                return false;
+        } else if (!loginname.equals(other.loginname))
+            return false;
+        return true;
+    }
+
+}
