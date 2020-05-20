cb1bcc9e90693fd2e0ee763abc7233a7911e7a1b
supermoewe <sandra.kiefer@student.hs-rm.de>
2020-05-20 09:51:22 +0200

    aufgabe04

--- src/main/java/de/hsrm/mi/web/bratenbank/benutzer/BenutzerServiceImpl.java
+++ src/main/java/de/hsrm/mi/web/bratenbank/benutzer/BenutzerServiceImpl.java
@@ -1,9 +1,13 @@
 package de.hsrm.mi.web.bratenbank.benutzer;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BenutzerServiceImpl implements BenutzerService {
+
+    @Autowired
+    BenutzerRepository benutzerrepro;
 
     @Override
     public boolean pruefeLogin(String loginname, String passwort) {
@@ -16,7 +20,29 @@ public class BenutzerServiceImpl implements BenutzerService {
 
     @Override
     public String ermittlePasswort(String loginname) {
-        return (loginname + loginname.length());
+        if(findeBenutzer(loginname) != null) {
+            return benutzerrepro.findByLoginname(loginname).getPasswort();
+        } else {
+            return (loginname + loginname.length());
+        }
+    }
+
+    @Override
+    public Benutzer registriereBenutzer(Benutzer neubenutzer) throws BenutzernameSchonVergeben {
+        if(findeBenutzer(neubenutzer.getLoginname()) == null) {
+            return benutzerrepro.save(neubenutzer);
+        } else {
+            throw new BenutzernameSchonVergeben();
+        }
+    }
+
+    @Override
+    public Benutzer findeBenutzer(String loginname) {
+        try {
+            return benutzerrepro.findByLoginname(loginname);
+        } catch (Exception e) {
+            return null;
+        }
     }
     
 }
---
+++ src/main/java/de/hsrm/mi/web/bratenbank/benutzer/BenutzernameSchonVergeben.java
@@ -0,0 +1,7 @@
+package de.hsrm.mi.web.bratenbank.benutzer;
+
+public class BenutzernameSchonVergeben extends RuntimeException {
+    
+    private static final long serialVersionUID = 1L;
+
+}
